6b0cae3cd1b56d81bc3beb7022d22ae4dc7e322d
Terry Cain <terry@terrys-home.co.uk>
2019-07-19 15:57:11 +0100

    Bump version: 1.3.0 → 2.0.0

--- sanic_json_logging/__init__.py
+++ sanic_json_logging/__init__.py
@@ -8,7 +8,7 @@ from sanic_json_logging.formatters import LOGGING_CONFIG_DEFAULTS
 from sanic_json_logging.sanic_app import NoAccessLogSanic
 
 
-__version__ = '1.3.0'
+__version__ = '2.0.0'
 __all__ = ['setup_json_logging', 'NoAccessLogSanic']
 
 
